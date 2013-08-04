1f5f1a2293a9401d38fd3d7b039889a9730a7dcd
Marco Schuster <marco+github@m-s-d.eu>
2013-08-04 04:54:40 +0200

    add client-side app and js

---
+++ app.js
@@ -0,0 +1,405 @@
+
+//event listener for messages from the feeditem-iframes
+window.addEventListener('message', function(event) {
+  //check if it's a mesage from us
+  if(!event.data.type)
+    return;
+  if(event.data.type=="seth") {
+    var pid=event.data.myId;
+    var h=event.data.scrollHeight;
+    $("#fl-"+pid+" iframe").height(h);
+  } else if(event.data.type=="keypress") {
+    var ve=jQuery.Event("keypress");
+    ve.keyCode=event.data.ev.keyCode;
+    ve.metaKey=event.data.ev.metaKey;
+    $(window).trigger(ve);
+  }
+},false);
+
+//tell server to reload all feeds from origin
+function updateFeed(id) {
+  $("#feed_update").attr("disabled","disabled");
+  $.getJSON("api.php?action=update&feed="+id,function(data) {
+    if(data.status!="ok") {
+      alert("Fehler in update(): "+data.message);
+      $("#feed_update").removeAttr("disabled");
+      return;
+    }
+    appstate.feed=0;
+    $(window).hashchange();
+    $("#feed_update").removeAttr("disabled");
+  });
+}
+
+//load a list of all feeds from server
+function loadFeedList() {
+  $.getJSON("api.php?action=getfeeds&feed=0",function(data) {
+    if(data.status!="ok") {
+      alert("Fehler in getfeeds(): "+data.message);
+      return;
+    }
+    $("#feedlist .feed").remove();
+    data.items.forEach(function(e){
+      if(e.icon=="")
+        e.icon="feed-icon-28x28.png";
+      var obj={
+        id:e.id,
+        unread:e.total-e.read,
+        title:e.title,
+        icon:e.icon,
+      };
+      if(e.read!=e.total)
+        obj.liclass="hasunread ";
+      else
+        obj.liclass=" ";
+      
+      if(e.icon!="")
+        obj.liclass+="hasicon ";
+      
+      var el=$($("#tpl-feedlist").jqote(obj));
+      el.appendTo($("#feedlist"));
+    });
+  });
+}
+
+//open a feed item in the list (pos: db id of the item)
+function openFeedItem(pos) {
+  $(".feedline.open").removeClass("open");
+  $("#fl-"+pos).addClass("open");
+  if($("#fl-"+pos).length>0)
+      $("#feedentries").scrollTo($("#fl-"+pos));
+  //actually populate the iframe we created earlier!
+  $("#fl-"+pos+" iframe").attr("src","data:text/html;charset=utf-8,"+$("#fl-"+pos).data("html"));
+}
+
+//load a new feed, or open an item of the current feed
+function loadFeed(id,pos) {
+  pos=pos||0;
+  if(appstate.feed!=id) {
+    console.log("appstate fid="+appstate.feed+", id="+id);
+    $("#feedtitle").html("Lade...");
+    $("#feedentries li.feedline").remove();
+    loadFeedData(id,pos,0);
+  } else {
+    console.log("appstate fid=id="+id);
+    openFeedItem(pos);
+    markAsRead(id,pos,true);
+    appstate.pos=pos;
+  }
+}
+
+//tell the backend that we read an item
+function markAsRead(fid,pid,read) {
+//return;
+  if(read)
+    var state="read";
+  else
+    var state="unread";
+  if(pid==0)
+    return;
+  $.getJSON("api.php?action=setreadstate&feed="+fid+"&item="+pid+"&state="+state,function(data) {
+    if(data.status!="ok") {
+      alert("Fehler in markAsRead(): "+data.message);
+      return;
+    }
+    console.log("updated readstate of "+fid+"/"+pid+" to "+state);
+    if(appstate.feed==fid)
+      $("#fl-"+pid+" .title").removeClass("unread");
+    if(data.affected==1) {
+      var old=parseInt($("#fi-"+fid+" .unread_count").html());
+      if(read)
+        old--;
+      else
+        old++;
+      $("#fi-"+fid+" .unread_count").html(old);
+      if(old==0)
+        $("#fi-"+fid).removeClass("hasunread");
+    }
+  });
+}
+
+//get the items of a feed
+function loadFeedData(id,pos,start) {
+  start=start||0;
+   $.getJSON("api.php?action=get&start="+start+"&feed="+id,function(data) {
+      if(data.status!="ok") {
+        alert("Fehler in getFeed("+id+"): "+data.message);
+        return;
+      }
+      $("#feed_title").html(data.feed.title);
+      if(data.feed.link!="")
+        $("#feed_href").attr("href",data.feed.link);
+      else
+        $("#feed_href").attr("href","");
+      
+      $("#feed_total").html(data.total);
+      
+      var fd=new Date(data.feed.lastread*1000);
+      var fds=dateFormat(fd,"dd.mm.yyyy HH:MM:ss");
+      $("#feed_ts").html(fds);
+      
+      data.items.forEach(function(e) {
+        var d=new Date(e.time*1000);
+        var ds=dateFormat(d,"dd.mm.yyyy HH:MM:ss");
+        var obj={
+          id:e.id,
+          title:e.title,
+          date:ds,
+          href:e.link,
+          fulltext:e.fulltext,
+          author:e.author,
+        };
+        if(e.timestamp===null)
+          obj.titleclass="unread";
+        else
+          obj.titleclass="";
+        
+        var el=$($("#tpl-feedline").jqote(obj));
+        $(".title",el).click(function(){
+            location.hash="feed/"+id+"/"+e.id;
+        });
+        
+        $("#feedmore").before(el);
+        /*
+        Evil hack. To provide XSS protection, create an iframe node.
+        Then, inject a script which posts a message to our window (lol, this is not covered by same-origin-policy)
+        which tells us the "inner" height of the content, so we can properly set the iframe's height to avoid
+        double scrollbars.
+        The content is supplied as a data-URL... which sets the source frame URL/protocol to NULL and so
+        provides the SOP protection. Also, it protects "our" DOM from being polluted with not properly closed tags and other bullsh*t.
+        The iframe content is set at opening-time above in the code.
+        */
+        var ifr=$("<iframe>");
+        ifr.attr("src","about:blank");
+        //Browsers like to set an implicit height on iframes (Chrome: 150px)
+        //So set it to 0. Upon loading the content, it will automatically scale up again :)
+        ifr.height(0);
+        ifr.load(function() {
+          var dd=this.contentDocument||this.contentWindow.document;
+          dd.body.innerHTML=e.fulltext;
+          
+        });
+        var sc="<scr"+"ipt type=\"text/javascript\">\n";
+        sc+="var theId="+e.id+";";
+        sc+=$("#inject-height").html();
+        sc+="</scr"+"ipt>\n";
+        e.fulltext+=sc;
+        ifr.appendTo($("#fl-"+e.id+" .fullText"));
+        $("#fl-"+e.id).data("html",encodeURIComponent(e.fulltext));
+        /* Evil hack ends here */
+        
+        if(e.timestamp===null)
+          $("#itemread-"+e.id).attr("checked","true");
+        
+        $("#feed_shown").html($(".feedline").length);
+        
+      });
+      appstate.feed=id;
+      appstate.pos=pos;
+      if(data.next) {
+        appstate.nextstart=data.next;
+        $("#feedmore").removeClass().addClass("more");
+      } else {
+        appstate.nextstart=0;
+        $("#feedmore").removeClass().addClass("nomore");
+      }
+      openFeedItem(pos);
+      markAsRead(id,pos,true);
+    });
+}
+
+//add a new feed to the backend
+function addFeed() {
+  $("#addnewfeed").attr("disabled","disabled");
+  var url=$("#newfeedurl").val();
+  console.log("loading feed "+url);
+  console.log(("api.php?action=add&feed="+encodeURIComponent(url)));
+  $.getJSON("api.php?action=add&feed="+encodeURIComponent(url),function(data) {
+    if(data.status!="ok") {
+      $("#addnewfeed").removeAttr("disabled");
+      alert("Fehler in addFeed("+id+"): "+data.message);
+    }
+    console.log("added feed");
+    console.log(data);
+    location.hash="feed/"+data.id;
+    updateFeed(data.id);
+    loadFeedList();
+    $("#addnewfeed").removeAttr("disabled");
+  });
+  
+}
+
+//Application state
+var appstate={
+  view:"index", /* the current view */
+  feed:0, /* current feed id */
+  pos:0, /* current feed item id */
+  nextstart:0,
+}
+
+jQuery(document).ready(function($){
+  //Event handler for the "Read more" in feedlist view
+  $("#feedmore").click(function() {
+    if(!$("#feedmore").hasClass("more"))
+      return;
+    $("#feedmore").removeClass().addClass("loading");
+    loadFeedData(appstate.feed,appstate.pos,appstate.nextstart);
+  });
+  
+  //Hash change handler for feedview
+  $(window).bind("hashchange",function() {
+    var h=location.hash;
+    var f=/feed\/([0-9]*)\//.exec(h);
+    if(!f) //we're not in feedview, let other handlers take a chance...
+      return;
+    appstate.view="feed";
+    $(".view").hide();
+    $("#feed").show();
+    //position
+    var p=/feed\/([0-9]*)\/([0-9]*)/.exec(h);
+    if(!p) 
+      loadFeed(f[1]);
+    else
+      loadFeed(f[1],p[2]);
+  });
+  
+  //hash change handler for index view
+  $(window).bind("hashchange",function() {
+    var h=location.hash;
+    var i=/index/.exec(h);
+    if(!i) //we're not in indexview, let other handlers take a chance...
+      return;
+    $(".view").hide();
+    $("#index").show();
+    appstate.view="index";
+  });
+  
+  //hash change handler for addnew or addgr view
+  $(window).bind("hashchange",function() {
+    var h=location.hash;
+    var op=/op\/(.*)/.exec(h);
+    if(!op) //we're not in feedview, let other handlers take a chance...
+      return;
+    console.log("op: "+op[1]);
+    $(".view").hide();
+    switch(op[1]) {
+      case "addnew":
+        $("#opaddnew").show();
+        break;
+      case "addgr":
+        $("#opaddgr").show();
+        break;
+    }
+    appstate.view=op[1];
+  });
+  
+  //read the current hash (e.g. when tab-clicking around or reloading)
+  $(window).hashchange();
+  
+  //these are handled externally, the spaghetti was too much
+  $("#addnewfeed").click(addFeed);
+  $("#importgrfile").click(importgrfile);
+  
+  //load the feed list
+  loadFeedList();
+  $("#feed_reload").click(function() {
+    appstate.feed=0; //force the feedhandler to reload the feed from the hash-supplied value. evil.
+    $(window).hashchange();
+  });
+  
+  //update the current feed
+  $("#feed_update").click(function() {
+    updateFeed(appstate.feed);
+  });
+});
+
+//import a google reader takeout OPML file
+function importgrfile() {
+  //  http://stackoverflow.com/a/4006992
+  // AJAX-send a file...
+  function shipOff(event) {
+    var result = event.target.result;
+    var fileName = $("#grfile").get(0).files[0].name;
+    $("#importgrfile").attr("disabled","disabled");
+    $.post('api.php?action=importgr&feed=0', { data: result, name: fileName }, function(data) {
+      if(data.status!="ok") {
+        $("#grresult").html($("#grresult").html()+"Fehler in importGR(): "+data.message);
+        $("#importgrfile").removeAttr("disabled");
+        return;
+      }
+      if(data.invalid!=0) {
+        $("#grresult").html($("#grresult").html()+data.message);
+        $("#importgrfile").removeAttr("disabled");
+        return;
+      }
+      var total=data.feeds.length;
+      var remaining=total;
+      $("#grcounter").show();
+      $("#grcounter_total").html(total);
+      $("#grcounter_cur").html(remaining);
+      $("#grresult").html($("#grresult").html()+total+" Feeds in OPML gefunden\n");
+      data.feeds.forEach(function(e) {
+        $.getJSON("api.php?action=add&feed="+encodeURIComponent(e),function(data2) {
+          remaining--;
+          $("#grcounter_cur").html(total-remaining);
+          if(remaining<=0)
+            $("#importgrfile").removeAttr("disabled");
+          if(data2.status!="ok") {
+            if(data2.type=="AlreadyPresentException")
+              return;
+            var m=data2.type;
+            if(data2.type!="XMLParseException")
+              m+="\n"+data2.message;
+            $("#grresult").html($("#grresult").html()+"Fehler in addFeed("+e+"): "+m+"\n");
+          }
+        });
+      });
+    });
+  }
+  $("#grresult").html("Übertrage OPML zur Auswertung\n");
+  var file = $("#grfile").get(0).files[0];
+  var reader = new FileReader();
+  reader.readAsText(file, 'UTF-8');
+  reader.onload = shipOff;
+}
+
+//handle key presses. for now, we're just interested in the space key
+$(window).keypress(function(e) {
+  console.log("keypress");
+  console.log(e);
+  if(e.keyCode==32 && e.metaKey==false) { //space = next element
+    if(appstate.view!="feed")
+      return;
+    console.log("got a space at "+appstate.pos);
+    if(appstate.pos!=0 && !$("#fl-"+appstate.pos).length) {
+      console.log("warning, current pos not visible");
+      return;
+    } else if(appstate.pos==0) {
+      var n=$(".feedline").first();
+    } else {
+      var n=$("#fl-"+appstate.pos).next();
+      //check if we have to scroll, or to go next element
+      var top=$("#fl-"+appstate.pos).position().top-$("#feedentries").position().top;
+      console.log("top:" +top);
+      var bottom=top+$("#fl-"+appstate.pos).outerHeight();
+      console.log("bottom: "+bottom);
+      if(bottom>$("#feedentries").height()-$("#feedmore").outerHeight()) {
+        $("#feedentries").scrollTop($("#feedentries").scrollTop()+$("#feedentries").height()/4);
+        return;
+      }
+    }
+    var nId=n.attr("id");
+    e.preventDefault();
+    if(nId=="feedmore") {
+      console.log("arrived at last element");
+      if(!$("#feedmore").hasClass("more"))
+        return;
+      $("#feedmore").click();
+    } else {
+      var r=/fl-([0-9]*)/.exec(nId);
+      console.log("going to "+r[1]);
+      location.hash="feed/"+appstate.feed+"/"+r[1];
+      
+    }
+  }
+});
---
+++ index.php
@@ -0,0 +1,132 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="min-width=1000px" />
+    <title>SKYRSS Reader</title>
+    <link rel="stylesheet" href="app.css" type="text/css" media="all" />
+    
+    <script type="text/javascript" src="jquery-1.8.3.min.js"></script>
+    <script type="text/javascript" src="jquery.ba-hashchange.min.js"></script>
+    <script type="text/javascript" src="date.format.js"></script>
+    <script type="text/javascript" src="jquery.scrollTo-min.js"></script>
+    <script type="text/javascript" src="jquery.jqote2.min.js"></script>
+    
+    <!-- jqote template for a feed item line -->
+    <script type="text/html" id="tpl-feedline">
+<![CDATA[
+<li id="fl-<%= this.id %>" class="feedline">
+  <table class="topRow">
+  <tr>
+    <td class="title <%= this.titleclass %>"><%= this.title %></td>
+    <td class="date"><%= this.date %></td>
+  </tr>
+  </table>
+  <div class="feedDetails" id="data-<%= this.id %>">
+    <div><a class="fullLink" target="_blank" href="<%= this.href %>">Seite öffnen</a></div>
+    <div class="author"><%= this.author %></div>
+    <div class="fullText"></div>
+  </div>
+  <table class="bottomRow">
+    <tr>
+      <td><input type="checkbox" class="itemRead" id="itemread-<%= this.id %>"/> <label for="itemread-<%= this.id %>">Als ungelesen markieren</label></td>
+      <td>Teilen</td>
+    </tr>
+  </table>
+</li>
+]]>
+    </script>
+    
+    <!-- jqote template for a feed list entry -->
+    <script type="text/html" id="tpl-feedlist">
+<![CDATA[
+<li class="feed <%= this.liclass %>" id="fi-<%= this.id %>">
+<% if(this.icon!="") { %>
+  <img src="<%= this.icon %>" class="icon" />
+<% } %>
+  <a href="#feed/<%= this.id %>/"><%= this.title %> (<span class="unread_count"><%= this.unread %></span>)</a>
+</li>
+]]>    
+    </script>
+    
+    
+    <script type="text/plain" id="inject-height">
+// This script gets injected into the content iframes.
+// It tells the parent its inner height so that the iframe can be exactly fit
+// and also tells the parent key presses to allow space-scrolling.
+// This is not possible from outside because of same-origin policy.
+window.onload=function() {
+  //tell parent our height
+  window.parent.postMessage({
+    type:"seth",
+    scrollHeight:document.body.scrollHeight,
+    myId:theId,
+  },'*');
+}
+
+window.onkeypress=function(a) {
+  //tell parent that a key was pressed (relay SPACE for scrolling)
+  console.log(a);
+  window.parent.postMessage({
+    type:"keypress",
+    ev: {
+      keyCode:a.keyCode,
+      metaKey:a.metaKey,
+    },
+  },'*');  
+}
+    </script>
+    <script type="text/javascript" src="app.js"></script>
+  </head>
+  <body>
+  <div id="menu">
+    <ul>
+      <li><a href="#index">Startseite</a></li>
+      <li><a href="#op/addnew">Neuen Feed hinzufügen</a></li>
+      <li><a href="#op/addgr">GoogleReader Import</a></li>
+    </ul>
+    <hr />
+    <ul id="feedlist">
+      <li class="feed">Lade...</li>
+    </ul>
+  </div>
+  <div id="content">
+    <div class="view" id="opaddnew">
+      <input id="newfeedurl" size="90" type="url" /> <button id="addnewfeed">Feed hinzufügen</button>
+    </div>
+    <div class="view" id="opaddgr">
+      <input id="grfile" type="file" size="90" /> <button id="importgrfile">Import starten</button>
+      <div id="grcounter"><span id="grcounter_cur">0</span> von <span id="grcounter_total">0</span> Feeds geladen</div>
+      <pre id="grresult"></pre>
+    </div>
+    <div class="view" id="index">
+      Bitte wählen Sie links einen Feed aus...
+    </div>
+    <div class="view" id="feed">
+      <div id="feedbar">
+        <table style="width:100%">
+        <tr>
+          <td><a id="feed_href" target="_blank"><h1 id="feed_title"></h1></a></td>
+          <td>Stand: <span id="feed_ts"></span></td>
+          <td><span id="feed_shown"></span>/<span id="feed_total"></span> Einträge</td>
+          <td><button id="feed_reload">Neu laden</button></td>
+          <td><button id="feed_update">Von Server laden</button></td>
+          <td>
+            <select>
+              <option>Neueste zuerst</option>
+            </select>
+          </td>
+        </tr>
+        </table>
+      </div>
+      <ul id="feedentries">
+        <li id="feedmore">
+          <span class="more">Mehr Einträge</span>
+          <span class="loading">Lade...</span>
+          <span class="nomore">Keine weiteren Einträge vorhanden</span>
+        </li>
+      </ul>
+    </div>
+  </div>  
+  </body>
+</html>
