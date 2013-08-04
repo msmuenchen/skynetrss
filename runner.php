424d4f3757514c3cadcc1c2bb149a415bc422ac3
Marco Schuster <marco+github@m-s-d.eu>
2013-08-04 04:55:23 +0200

    add update cronjob runner

---
+++ runner.php
@@ -0,0 +1,25 @@
+<?
+require("config.php");
+require("DB.php");
+require("DB_Query.php");
+require("rss.php");
+
+$q=new DB_Query("select * from feeds where lastread<(UNIX_TIMESTAMP(NOW())-(ttl*60))");
+echo "Got ".$q->numRows." feeds\n";
+
+while($r=$q->fetch()) {
+  echo "Updating ".$r["url"]."\n";
+  $log="Feed: ".$r["url"]."\n";
+  $success=0;
+  try {
+    updateFeed($r["id"]);
+    $log.="Done\n";
+    $success=1;
+  } catch(Exception $e) {
+    $log.="Error ".get_class($e).": ".$e->getMessage()."\n";
+  }
+  
+  $q2=new DB_Query("insert into feed_runs set feed_id=?,timestamp=UNIX_TIMESTAMP(NOW()),data=?,success=?",$r["id"],$log,$success);
+  echo "Done\n";
+}
+echo "Done with feeds\n";
