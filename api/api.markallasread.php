e5f62a66e59f07d50fb0ea668435b2a72334428c
Marco Schuster <marco+github@m-s-d.eu>
2013-09-01 08:45:41 +0200

    API endpoint markallasread: make multi-user aware

--- api/api.markallasread.php
+++ api/api.markallasread.php
@@ -1,8 +1,19 @@
 <?
+session_start();
+if(!isset($_SESSION["user"]))
+  $uid=0;
+else
+  $uid=$_SESSION["user"]["id"];
+
 $q=new DB_Query("select * from feeds where id=?",$feed);
 if($q->numRows!=1)
   throw new Exception("Feed ID invalid");
 
-$q=new DB_Query("INSERT INTO feed_read (feed_read.user_id,feed_read.feed_id,feed_read.item_id,feed_read.timestamp) SELECT ?,fi.feed_id,fi.id,UNIX_TIMESTAMP(NOW()) FROM `feed_items` as fi left join feed_read as fr on fr.feed_id=fi.feed_id and fr.item_id=fi.id WHERE fi.feed_id=? and fr.timestamp IS NULL",$feed,$feed);
+$q=new DB_Query(
+  "INSERT INTO feed_read (feed_read.user_id,feed_read.feed_id,feed_read.item_id,feed_read.timestamp) 
+     SELECT ?,fi.feed_id,fi.id,UNIX_TIMESTAMP(NOW())
+     FROM `feed_items` AS fi
+     LEFT JOIN feed_read AS fr ON fr.feed_id=fi.feed_id AND fr.item_id=fi.id AND fr.user_id=?
+     WHERE fi.feed_id=? AND fr.timestamp IS NULL",$uid,$uid,$feed);
 $ret["affected"]=$q->affectedRows;
 //no confirmation checking to keep down execution cost!
