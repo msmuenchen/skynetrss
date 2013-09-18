699a1054d331c66d14041408f6cc2c28a803ee79
Marco Schuster <marco+github@m-s-d.eu>
2013-09-18 03:46:46 +0200

    console: add gerror method extending console.error

--- js/console.js
+++ js/console.js
@@ -10,3 +10,14 @@ console.glog=function() {
   if(console.group)
     console.groupEnd();
 }
+
+//first argument is group, rest gets forwarded to console.error
+console.gerror=function() {
+  var grp=arguments[0];
+  var args = Array.prototype.slice.call(arguments, 1); //http://stackoverflow.com/a/6808662/1933738
+  if(console.group)
+    console.group(grp);
+  console.error.apply(this,args);
+  if(console.group)
+    console.groupEnd();
+}
