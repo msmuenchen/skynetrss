d1ba5634a0c3d8deb709c47cb5d87eaf113e43cb
Marco Schuster <marco+github@m-s-d.eu>
2013-09-19 02:50:09 +0200

    updated english i18n

--- i18n/en.js
+++ i18n/en.js
@@ -50,4 +50,58 @@ xlateAddLang("en","English",{
   page_more:"More...",
   page_nomore:"No more entries left",
   page_delete:"Delete",
+  page_library:"Library",
+  page_createaccount:"Create account",
+  page_jumponopen:"Jump to the top upon opening a feed item",
+  page_language:"UI language",
+  page_detectfeeds:"Discover feeds",
+  page_welcome:"Welcome",
+  page_firststeps:"First steps",
+  page_add:"Add",
+  tag_german:"German",
+  tag_blog:"Blogs",
+  tag_news:"News",
+  tag_fun:"Fun",
+  tag_english:"English",
+  tag_technology:"Technology",
+  tag_comic:"Comics",
+  tag_itsec:"IT security",
+  tag_medicine:"Medicine",
+  page_firststeps_1:"User interface:\
+<p>On the left side is the central navigation. Here you can switch between the feeds you have subscribed to, as well as launch other actions.</p>\
+<p>On the right side the content will be shown.</p>\
+",
+  page_firststeps_2:"Discover feeds in the library:\
+<p>The library is grouped by categories and a small part of the feeds available on this server.</p>\
+<p>If you like a feed, just click on \"Subbscribe\" to add it to your subscriptions.</p>\
+",
+  page_firststeps_3:"OPML-Import:\
+<p>With the function \"OPML-Import\" you can quickly import OPML files from Google Reader, Feedly and other compatible RSS readers.</p>\
+<p>Choose the file from your computer and start the import by clicking on \"Start import\".</p>\
+<p>All valid feeds in the OPML will be added to your subscriptions.</p>\
+<p>In the \"Status\" column you can see the import status of the feeds.</p>\
+<p>The most common errors \"Server problem\" and \"Invalid XML\" can not be fixed by us, because they're caused by the feed source.</p>\
+<p>Please note that any feed with errors is <em>not</em> added to your library.</p>\
+",
+  page_firststeps_4:"Manually subscribe to a feed:\
+<p>With the function \"Add feed\" you can add feeds from websites by hand.</p>\
+<p>Just paste the URL of the feed (if known) or the URL of the website (e.g. \"spiegel.de\") and click on \"Discover feeds\".</p>\
+<p>The program will now show you a list of the feeds discovered at this location. To add a specific feed, click on \"Add feed\".</p>\
+",
+  page_firststeps_5:"Settings:\
+<p>In the settings you can manage your feed subscriptions, change display options and (for SkyRSS accounts) change your password.</p>\
+<p>The display option \"Jump to the top upon opening a feed item\" determines, if the top of a feed item you open will jump to the window top (so you can read as much as possible at once) or if it is \
+supposed to stay at the position where you opened it.</p>\
+",
+  page_firststeps_6:"Feed view:\
+<p>In the feed view the items available in a feed are shown.</p>\
+<p>You can open items by clicking on them.</p>\
+<p>Items marked in bold are currently marked unread; as soon as you open them, they'll automatically be marked read.</p>\
+<p>\"Reload\" will get the newest items from the servers; \"Reload from source\" forces a full update from the feed source.</p>\
+<p>You can mark individual items as unread by clicking on the \"Mark as read\" button and share them with your Twitter followers and Facebook friends by clicking on \"Share\".</p>\
+<p>You can customize the text for Twitter and Facebook posts before publishing.</p>\
+<p>If you have a Premium account, certain feeds will appear in full text. This is a premium feature because some feed publishers choose to offer only an excerpt of the content and loading \
+and storage of the full content is a very time- and memory-intensive effort.</p>\
+<p>If you have a Premium account and would like to read a feed in full text, please write us an e-mail and we will take care of it.</p>\
+",
 });
