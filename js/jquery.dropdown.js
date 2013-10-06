5fffde6568f5077666aa26dd3bf949d25c5dea33
Marco Schuster <marco+github@m-s-d.eu>
2013-10-06 04:28:29 +0200

    dropdown.js: collapse the menu after clicking anywhere (like a real menu)

--- js/jquery.dropdown.js
+++ js/jquery.dropdown.js
@@ -1,10 +1,13 @@
 $.fn.extend({
         dropdown : function() {
+                var dd=[];
                 this.each(function() {
                         var e=$(this);
+                        dd.push(e);
                         var h=$(".dropdown-header",e);
                         var i=$(".dropdown-items",e);
-                        h.click(function() {
+                        h.click(function(ev) {
+                                ev.stopPropagation();
                                 if(e.hasClass("open"))
                                         i.slideUp(500,function() { e.removeClass("open") });
                                 else {
@@ -13,6 +16,15 @@ $.fn.extend({
                                         i.slideDown(500);
                                 }
                                 
+                        });
+                });
+                $(document).click(function() {
+                        console.glog("dropdown","document clicked");
+                        dd.forEach(function(e) {
+                                console.glog("dropdown","collapsing",e);
+                                var i=$(".dropdown-items",e);
+                                if(e.hasClass("open"))
+                                        i.slideUp(500,function() { e.removeClass("open") });
                         });
                 });
         }
