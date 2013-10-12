643a86d2e7776025caa4b5f46e3b59e8ba19a022
Marco Schuster <marco+github@m-s-d.eu>
2013-10-13 01:34:56 +0200

    add jquery alterclass plugin

--- index.php
+++ index.php
@@ -29,6 +29,7 @@ if(isset($_GET["debug"]) && $config["debugurl"]!="")
     <script type="text/javascript" src="js/jquery.jqote2.min.js"></script>
     <script type="text/javascript" src="js/jquery.toggleCheckbox.js"></script>
     <script type="text/javascript" src="js/jquery.dropdown.js"></script>
+    <script type="text/javascript" src="js/jquery.alterclass.js"></script>
     <script type="text/javascript" src="js/jquery-ui-1.10.3.custom.js"></script>
     
     <script type="text/javascript" src="config.js"></script>
