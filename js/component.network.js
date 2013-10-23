2da0793b225d647c8e9cb5f346ed8373aa6981e8
Marco Schuster <marco+github@m-s-d.eu>
2013-10-23 03:19:30 +0200

    network: on first run always run the online/offline events

--- js/component.network.js
+++ js/component.network.js
@@ -8,20 +8,22 @@ function checkNetworkConnection() {
   console.glog("checkNetworkConnection","checking online status, current online=",appstate.online);
   doAPIRequest("onlinecheck",{ignoreNetworkException:true},function() { //success
     console.glog("checkNetworkConnection","we're online");
-    if(appstate.online==false) {
+    if(appstate.online==false || appstate.online==null) {
+      appstate.online=true;
       console.glog("checkNetworkConnection","going online");
       $(document).trigger("skyrss_netonline");
-    }
-    appstate.online=true;
+    } else //this doubling is intentional, the triggers may require API access and thus need appstate.online
+      appstate.online=true;
     $("#offline-box").hide();
   },
   function() { //fail
     console.glog("checkNetworkConnection","we're apparently offline");
-    if(appstate.online==true) {
+    if(appstate.online==true || appstate.online==null) {
+      appstate.online=false;
       console.glog("checkNetworkConnection","going offline");
       $(document).trigger("skyrss_netoffline");
-    }
-    appstate.online=false;
+    } else
+      appstate.online=false;
   },
   null //always
   );
@@ -29,7 +31,7 @@ function checkNetworkConnection() {
 
 //the first thing we have to do is to check if we're online!
 $(document).ready(function() {
-  appstate.online=false;
+  appstate.online=null; //not true/false, so that the check always fires a trigger on first execution
   checkNetworkConnection();
   setInterval(checkNetworkConnection,1000*5); //check every two minutes if we're online
   $("#offline-box").click(checkNetworkConnection);
