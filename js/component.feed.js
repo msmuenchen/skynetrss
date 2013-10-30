108709fc09f7d04b17f3484772a4b9b7437aef18
Marco Schuster <marco+github@m-s-d.eu>
2013-10-30 17:28:50 +0100

    component.feed: add basic offline support

--- js/component.feed.js
+++ js/component.feed.js
@@ -1,5 +1,39 @@
 //SkyRSS components: feed/feeditem
 
+$(document).ready(function() {
+  console.glog("component.feed","initializing");
+  appstate.feedDB={};
+  appstate.feedDB.ready=false;
+  appstate.feedDB.supported=false;
+  if(!Modernizr.indexeddb && !Modernizr.websqldatabase) {
+    console.glog("component.feed","cannot store feed data offline");
+    return;
+  }
+  appstate.feedDB.supported=true;
+  $.indexedDB("skyrss",{
+    version:2,
+    schema:{
+      1:function(t) {
+        console.glog("component.feed","creating database");
+        t.createObjectStore("feeds",{keyPath:"id"});
+        t.createObjectStore("feeditems",{keyPath:["id","feed_id"]});
+      },
+      2:function(t) {
+        console.glog("component.feed","creating lookup index for timestamp");
+        var os=t.objectStore("feeditems");
+        os.createIndex("idx_ts",["id","feed_id","time"]);
+      },
+    }
+  }).done(function() {
+    console.glog("component.feed","database opened");
+    appstate.feedDB.ready=true;
+  }).progress(function() {
+    console.glog("component.feed","db open in progress");
+  }).fail(function(err,e) {
+    console.gerror("component.feed","db open failed:",err,e);
+  });
+});
+
 $(document).on("skyrss_feed_requestupdate",function(ev,a) {
   if(appstate.online==null) {
     setTimeout(arguments.callee.bind(this,ev,args),500);
@@ -57,7 +91,7 @@ $(document).on("skyrss_feed_data_request",function(e,a) {
   if(appstate.online==true) {
     loadFeedFromServer(a);
   } else {
-    alert("no feed loading in offline mode");
+    loadFeedFromDB(a);
   }
 });
 
@@ -75,7 +109,78 @@ function loadFeedFromServer(a) {
       return;
     }
     $(document).trigger("skyrss_feed_data_done",data);
+    if(!appstate.feedDB.supported)
+      return;
+    if(!appstate.feedDB.ready) {
+      console.gerror("component.feed","cannot insert data into DB, not opened yet");
+      return;
+    }
+    var tp=$.indexedDB("skyrss").transaction("feeditems");
+    tp.progress(function(t) {
+      var os=t.objectStore("feeditems");
+      data.items.forEach(function(e) {
+        console.glog("component.feed","inserting item",e.id);
+        var p=os.put(e);
+        p.fail(function(a,b){
+          console.gerror("component.feed","insert error",a,b);
+        }).done(function(){
+          console.glog("component.feed","insert done");
+        });
+      });
+    }).fail(function(e) {
+      console.gerror("component.feed","insert transaction failed",e);
+    }).done(function() {
+      console.glog("component.feed","insert txn done");
+    });
   });
 }
-function loadFeedFromDB(id,start,len) {
+function loadFeedFromDB(a) {
+  if(!appstate.feedDB.supported)
+    return;
+  //todo this sucks
+  if(appstate.feedlist.loaded!=true) {
+    setTimeout(arguments.callee.bind(this,ev,args),500);
+    console.glog("component.feed","waiting for preconditions");
+    return;
+  }
+  if(!appstate.feedDB.ready) {
+    console.gerror("component.feed","cannot read from DB, not opened yet");
+    return;
+  }
+
+//todo: implement paging, unread, sort, start, len
+  console.glog("component.feed","requesting DB data for feed",a.feed);
+  var tp=$.indexedDB("skyrss").transaction("feeditems");
+  var ret=[];
+  tp.progress(function(t) {
+    var os=t.objectStore("feeditems");
+    os.each(function(item) {
+      if(item.value.feed_id!=a.feed)
+        return;
+      ret.push(item.value);
+    });
+  }).fail(function(e) {
+    console.gerror("component.feed","read txn failed",e);
+  }).done(function() {
+    console.glog("component.feed","read txn done",ret);
+    var fo=null;
+    appstate.feedlist.object.items.forEach(function(e){
+      if(e.id==a.feed)
+        fo=e;
+    });
+    if(fo==null) {
+      console.gerror("component.feed","unable to find feed object");
+      return;
+    }
+    var retObj={
+      items:ret,
+      next:0,
+      remain:0,
+      total:ret.length,
+      feed:fo,
+    };
+    console.log("retobj",retObj);
+    $(document).trigger("skyrss_feed_data_done",retObj);
+  });
 }
+
